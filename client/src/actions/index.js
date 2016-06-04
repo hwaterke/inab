e3fc74da8373847ebe1082f41a326e1884b0b5b2
hwaterke <hwaterke@users.noreply.github.com>
2016-06-04 14:29:01 +0200

    Cleaned old actions

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -1,27 +1,4 @@
-// Contains the action creators.
 import * as types from './types';
-
-// TODO ID should be determined by the server.
-let nextTodoId = 0;
-
-export const addTransaction = (date, payee, category, description, amount) => {
-  return {
-    type: types.ADD_TRANSACTION,
-    id: nextTodoId++,
-    date,
-    payee,
-    category,
-    description,
-    amount
-  };
-};
-
-export const addRandomTransaction = () => {
-  const names = ['Alice', 'Bob', 'Eve'];
-  var payee = names[Math.floor(Math.random() * names.length)];
-  const amount = Math.round(Math.random() * 500);
-  return addTransaction("12/12/2016", payee, "Category", "Description", amount);
-};
 
 export const selectTransaction = (id) => {
   return {
