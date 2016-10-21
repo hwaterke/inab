4abacc56c14b445ff5e04d79932f58d97d4a8c0b
hwaterke <hwaterke@users.noreply.github.com>
2016-10-21 16:57:37 +0200

    Cleaned BudgetItemForm

--- client/src/components/BudgetItemForm.js
+++ client/src/components/BudgetItemForm.js
@@ -1,13 +1,9 @@
 import React from 'react';
 import { Field, reduxForm } from 'redux-form';
-import ui from 'redux-ui';
 
-@ui()
 class BudgetItemForm extends React.Component {
   static propTypes = {
-    handleSubmit: React.PropTypes.func.isRequired,
-    ui: React.PropTypes.object.isRequired,
-    updateUI: React.PropTypes.func.isRequired
+    handleSubmit: React.PropTypes.func.isRequired
   }
 
   render() {
