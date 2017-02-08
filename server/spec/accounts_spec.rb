8d4ec434bda829e792350f1359c82027f824601f
hwaterke <hwaterke@users.noreply.github.com>
2017-02-08 22:25:41 +0100

    Added a test for the backend

--- server/spec/accounts_spec.rb
+++ server/spec/accounts_spec.rb
@@ -1,4 +1,4 @@
-require "rack/test"
+require 'rack/test'
 
 OUTER_APP = Rack::Builder.parse_file('config.ru').first
 
@@ -13,7 +13,15 @@ describe INAB::API do
     it 'returns an empty array of accounts' do
       get '/api/accounts'
       expect(last_response.status).to eq(200)
-      expect(JSON.parse(last_response.body)).to eq({"data" => []})
+      expect(JSON.parse(last_response.body)).to eq({'data' => []})
+    end
+  end
+
+  context 'POST /api/accounts' do
+    it 'returns the created account' do
+      post '/api/accounts', {name: 'MyAccount'}
+      expect(last_response.status).to eq(201)
+      expect(JSON.parse(last_response.body)).to eq({'id' => 1, 'name' => 'MyAccount'})
     end
   end
 end
