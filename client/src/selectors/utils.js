c0a106d171c8ee13f0e4fc87c67f9f215e40f5ef
hwaterke <hwaterke@users.noreply.github.com>
2017-03-13 22:55:37 +0100

    Removed unused selector util

--- client/src/selectors/utils.js
+++ client/src/selectors/utils.js
@@ -25,16 +25,6 @@ const mapByKey = (items, propName) => {
   return result;
 };
 
-/**
- * Implementation of map for Map objects.
- * Each value is mapped by the provided mapper function.
- */
-export const mapMap = (aMap, mapper) => {
-  const result = new Map();
-  aMap.forEach((value, key) => result.set(key, mapper(value, key)));
-  return result;
-};
-
 export const beginningOfMonth = (dateString) => moment(dateString).startOf('month').format('YYYY-MM-DD');
 
 export const createMappingSelector = (itemSelector, propName) => createSelector(itemSelector, (items) => mapByKey(items, propName));
