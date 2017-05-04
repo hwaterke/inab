a6e7b677150a167b2288cf09b5fee4b743566a03
hwaterke <hwaterke@users.noreply.github.com>
2017-05-04 13:58:08 +0200

    Changed format for date picker

--- client/src/components/forms/fields/DatePickerField.js
+++ client/src/components/forms/fields/DatePickerField.js
@@ -3,12 +3,14 @@ import DatePicker from 'react-datepicker';
 import 'react-datepicker/dist/react-datepicker.css';
 import moment from 'moment';
 
-const DatePickerField = ({input}) =>
+const DatePickerField = ({input}) => (
   <DatePicker
+    dateFormat="DD/MM/YYYY"
     className="form-control"
     selected={moment(input.value)}
     onChange={param => input.onChange(param.format('YYYY-MM-DD'))}
-  />;
+  />
+);
 
 DatePickerField.propTypes = {
   input: React.PropTypes.shape({
