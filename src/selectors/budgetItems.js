4b1bf17ff677364c625ba014c0da50f3a61d57c7
hwaterke <hwaterke@users.noreply.github.com>
2017-05-24 16:15:48 +0200

    Typed some BudgetItem selectors

--- src/selectors/budgetItems.js
+++ src/selectors/budgetItems.js
@@ -4,23 +4,24 @@ import {createSelector} from 'reselect';
 import {arraySelector} from 'hw-react-shared';
 import {createInMonthSelectors, createUpToMonthSelectors, sumOfAmounts} from './utils';
 import {BudgetItemResource} from '../entities/BudgetItem';
+import type {BudgetItem} from '../entities/BudgetItem';
 
 export const budgetItemsInMonth = createInMonthSelectors(
   arraySelector(BudgetItemResource),
-  bi => bi.month
+  (bi: BudgetItem) => bi.month
 );
 
 export const budgetItemsUpToMonth = createUpToMonthSelectors(
   arraySelector(BudgetItemResource),
-  bi => bi.month
+  (bi: BudgetItem) => bi.month
 );
 
 export const getSelectedMonthBudgetItemByCategoryId = createSelector(
   budgetItemsInMonth.selected,
-  budgetItems => R.map(R.head)(R.groupBy(R.prop('category_uuid'), budgetItems))
+  (budgetItems: BudgetItem[]) => R.map(R.head)(R.groupBy(R.prop('category_uuid'), budgetItems))
 );
 
 export const getBudgetItemsSumUpToPreviousMonth = createSelector(
   budgetItemsUpToMonth.previous,
-  budgetItems => sumOfAmounts(budgetItems)
+  (budgetItems: BudgetItem[]) => sumOfAmounts(budgetItems)
 );
