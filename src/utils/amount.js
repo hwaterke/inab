8c83047517d40002ac744edead9720bbfce01e59
hwaterke <hwaterke@users.noreply.github.com>
2017-05-24 16:12:48 +0200

    Typed amount conversion utils

--- src/utils/amount.js
+++ src/utils/amount.js
@@ -1,11 +1,13 @@
-export const amountToCents = amount => {
+// @flow
+
+export const amountToCents = (amount: number) => {
   if (amount) {
     return Math.round(Number(amount) * 100);
   }
   return 0;
 };
 
-export const amountFromCents = amount => {
+export const amountFromCents = (amount: number) => {
   if (amount) {
     return amount / 100;
   }
