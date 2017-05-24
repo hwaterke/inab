9f7a01b52f2ccddad585e6ea741bffa7cdf063ce
hwaterke <hwaterke@users.noreply.github.com>
2017-05-24 16:18:00 +0200

    Typed test utils

--- tests/selectors/utils.js
+++ tests/selectors/utils.js
@@ -1,26 +1,30 @@
+// @flow
 import reduxCrud from 'redux-crud';
+import type {CategoryGroup} from '../../src/entities/CategoryGroup';
 import {CategoryGroupResource} from '../../src/entities/CategoryGroup';
+import type {Category} from '../../src/entities/Category';
 import {CategoryResource} from '../../src/entities/Category';
+import type {BudgetItem} from '../../src/entities/BudgetItem';
 import {BudgetItemResource} from '../../src/entities/BudgetItem';
 
-export const createCategoryGroup = (store, uuid, name, priority) => {
-  const item = {uuid, name, priority};
+export const createCategoryGroup = (store: any, uuid: string, name: string, priority: number) => {
+  const item: CategoryGroup = {uuid, name, priority};
   store.dispatch(
     reduxCrud.actionCreatorsFor(CategoryGroupResource.path, {key: 'uuid'}).createSuccess(item)
   );
   return item;
 };
 
-export const createCategory = (store, uuid, name, priority, categoryGroupUuid) => {
-  const item = {uuid, name, priority, category_group_uuid: categoryGroupUuid};
+export const createCategory = (store: any, uuid: string, name: string, priority: number, categoryGroupUuid: string) => {
+  const item: Category = {uuid, name, priority, category_group_uuid: categoryGroupUuid};
   store.dispatch(
     reduxCrud.actionCreatorsFor(CategoryResource.path, {key: 'uuid'}).createSuccess(item)
   );
   return item;
 };
 
-export const createBudgetItem = (store, uuid, month, category_uuid, amount) => {
-  const item = {uuid, month, category_uuid, amount};
+export const createBudgetItem = (store: any, uuid: string, month: string, category_uuid: string, amount: number) => {
+  const item: BudgetItem = {uuid, month, category_uuid, amount};
   store.dispatch(
     reduxCrud.actionCreatorsFor(BudgetItemResource.path, {key: 'uuid'}).createSuccess(item)
   );
