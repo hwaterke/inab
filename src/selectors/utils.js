ecad95cfaf4a143ec17fd7d2774ab76d5ff3a637
hwaterke <hwaterke@users.noreply.github.com>
2017-05-24 16:17:24 +0200

    Typed some selector utility functions

--- src/selectors/utils.js
+++ src/selectors/utils.js
@@ -4,11 +4,14 @@ import moment from 'moment';
 import {createSelector} from 'reselect';
 import {getPreviousMonthMoment, getSelectedMonthMoment, getNextMonthMoment} from './month';
 
-type sumOfAmountsType = (items: {amount: number}[]) => number;
+type sumOfAmountsType = <T: {amount: number}>(items: T[]) => number;
 export const sumOfAmounts: sumOfAmountsType = R.reduce(
   (acc: number, record: {amount: number}) => acc + record.amount,
   0
 );
+
+export const beginningOfMonth = (dateString: string): string =>
+  moment(dateString).startOf('month').format('YYYY-MM-DD');
 
 // Creates a selector that filters a list of items for a selected month
 const createInMonthSelector = (itemSelector, itemMonthMapper, monthSelector) =>
