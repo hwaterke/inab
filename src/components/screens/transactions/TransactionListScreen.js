c2c1fa7a1390e07206d8ca523d0a27b8bb8d6736
hwaterke <hwaterke@users.noreply.github.com>
2017-07-14 16:14:13 +0200

    Added section headers to the transaction list

--- src/components/screens/transactions/TransactionListScreen.js
+++ src/components/screens/transactions/TransactionListScreen.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import {View, FlatList} from 'react-native';
+import {View, SectionList, Text, StyleSheet} from 'react-native';
 import {connect} from 'react-redux';
 import {TransactionResource} from 'inab-shared';
+import moment from 'moment';
+import R from 'ramda';
 import {crud} from '../../hoc/crud';
 import {globalStyles} from '../../../constants/styles';
 import {getTransactionForRendering} from '../../../selectors/transactions';
 import {TransactionRow} from './TransactionRow';
+import {colors} from '../../../constants/colors';
 
 const mapStateToProps = state => ({
   transactions: getTransactionForRendering(state)
@@ -42,10 +45,18 @@ export class TransactionListScreen extends React.Component {
 
   filterTransactions = transactions => {
     const {accountUuid} = this.props.navigation.state.params;
-    this.setState({
-      filteredTransactions: transactions.filter(
-        tr => !accountUuid || tr.account_uuid === accountUuid
+
+    const sections = Object.entries(
+      R.groupBy(
+        tr => moment(tr.date, 'YYYY-MM-DD').format('D MMMM YYYY'),
+        transactions.filter(
+          tr => !accountUuid || tr.account_uuid === accountUuid
+        )
       )
+    ).map(en => ({title: en[0], data: en[1]}));
+
+    this.setState({
+      filteredTransactions: sections
     });
   };
 
@@ -63,16 +74,37 @@ export class TransactionListScreen extends React.Component {
     }
   }
 
+  renderItem = ({item}) => <TransactionRow transaction={item} />;
+
+  renderSectionHeader = ({section}) =>
+    <View style={styles.sectionView}>
+      <Text style={styles.sectionText}>
+        {section.title}
+      </Text>
+    </View>;
+
   render() {
     return (
       <View style={globalStyles.screen}>
-        <FlatList
-          data={this.state.filteredTransactions}
+        <SectionList
+          sections={this.state.filteredTransactions}
           onRefresh={this.onRefresh}
           refreshing={this.state.isFetching}
-          renderItem={({item}) => <TransactionRow transaction={item} />}
+          renderItem={this.renderItem}
+          renderSectionHeader={this.renderSectionHeader}
         />
       </View>
     );
   }
 }
+
+const styles = StyleSheet.create({
+  sectionView: {backgroundColor: colors.banner, paddingVertical: 2},
+
+  sectionText: {
+    fontSize: 16,
+    fontWeight: 'bold',
+    color: 'white',
+    paddingLeft: 16
+  }
+});
