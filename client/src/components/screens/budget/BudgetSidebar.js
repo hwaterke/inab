950abb26db35d64664eebf5571d926a4722da624
hwaterke <hwaterke@users.noreply.github.com>
2017-09-15 11:39:10 +0200

    Added a button to quickly budget for underfunded categories.

--- client/src/components/screens/budget/BudgetSidebar.js
+++ client/src/components/screens/budget/BudgetSidebar.js
@@ -2,32 +2,88 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import ui from 'redux-ui';
 import {connect} from 'react-redux';
-import {byIdSelector} from 'hw-react-shared';
+import {arraySelector, byIdSelector} from 'hw-react-shared';
 import {
+  BudgetItemResource,
   CategoryResource,
+  getAvailableByCategoryIdForSelectedMonth,
+  getAvailableToBudget,
   getBudgetedThisMonth,
+  getSelectedMonthBudgetItemByCategoryId,
+  getSelectedMonthMoment,
   getToBeBudgetedSumInSelectedMonth
 } from 'inab-shared';
 import Amount from '../../Amount';
+import {crud} from '../../../hoc/crud';
 
 const mapStateToProps = state => ({
+  categories: arraySelector(CategoryResource)(state),
   categoriesById: byIdSelector(CategoryResource)(state),
+  availableToBudget: getAvailableToBudget(state),
+  availableByCategory: getAvailableByCategoryIdForSelectedMonth(state),
+  selectedMonth: getSelectedMonthMoment(state),
+  selectedMonthBudgetItemByCategoryId: getSelectedMonthBudgetItemByCategoryId(state),
   budgetedThisMonth: getBudgetedThisMonth(state),
   inflowInCurrentMonth: getToBeBudgetedSumInSelectedMonth(state)
 });
 
+@crud
 @ui()
 @connect(mapStateToProps)
 export class BudgetSidebar extends React.Component {
   static propTypes = {
+    categories: PropTypes.arrayOf(CategoryResource.propType).isRequired,
+    categoriesById: PropTypes.objectOf(CategoryResource.propType).isRequired,
+    availableToBudget: PropTypes.number.isRequired,
+    availableByCategory: PropTypes.instanceOf(Map).isRequired,
+    selectedMonth: PropTypes.object.isRequired,
+    selectedMonthBudgetItemByCategoryId: PropTypes.objectOf(BudgetItemResource.propType).isRequired,
     budgetedThisMonth: PropTypes.number.isRequired,
     inflowInCurrentMonth: PropTypes.number.isRequired,
-    categoriesById: PropTypes.objectOf(CategoryResource.propType).isRequired,
     selectedCategoryId: PropTypes.string,
+    createResource: PropTypes.func.isRequired,
+    updateResource: PropTypes.func.isRequired,
     ui: PropTypes.shape({
       categorySelected: PropTypes.string
     }).isRequired,
     updateUI: PropTypes.func.isRequired
+  };
+
+  /**
+   * Adds the specified amount to the category
+   */
+  budgetCategoryAdd = (categoryUuid, amount) => {
+    const existingBudgetItem = this.props.selectedMonthBudgetItemByCategoryId[categoryUuid];
+
+    if (existingBudgetItem) {
+      // Update
+      return this.props.updateResource(BudgetItemResource, {
+        ...existingBudgetItem,
+        amount: amount + existingBudgetItem.amount
+      });
+    } else {
+      // Create
+      return this.props.createResource(BudgetItemResource, {
+        month: this.props.selectedMonth.format('YYYY-MM-DD'),
+        category_uuid: categoryUuid,
+        amount
+      });
+    }
+  };
+
+  quickBudgetUnderfunded = async () => {
+    let availableToBudget = this.props.availableToBudget;
+
+    for (let category of this.props.categories) {
+      const available = this.props.availableByCategory.get(category.uuid);
+      if (available < 0) {
+        const adding = Math.min(-available, availableToBudget);
+        if (adding > 0) {
+          await this.budgetCategoryAdd(category.uuid, adding);
+          availableToBudget -= adding;
+        }
+      }
+    }
   };
 
   render() {
@@ -49,6 +105,10 @@ export class BudgetSidebar extends React.Component {
               <Amount amount={this.props.inflowInCurrentMonth} />
             </h2>
           </div>
+
+          <button onClick={this.quickBudgetUnderfunded} className="btn btn-primary">
+            Quick budget underfunded
+          </button>
         </div>
       );
     }
