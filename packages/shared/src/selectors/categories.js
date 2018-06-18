5e9feec840112fd557987e68f390c9588844f45a
Antonio Calapez <antonio.root@gmail.com>
2018-06-18 18:42:11 +0200

    Document the getCategorySuggestion function

--- packages/shared/src/selectors/categories.js
+++ packages/shared/src/selectors/categories.js
@@ -13,9 +13,17 @@ export const selectCategoriesByGroupId = createSelector(
     groupBy(prop('category_group_uuid'), sortBy(prop('priority'), categories))
 )
 
+/**
+ * This function suggests a category for a payee based on your last transactions
+ *
+ * It returns the last used category for that payee
+ *
+ * @param {*} payee: Value of a payee
+ * @param {*} selectedCategory: Value of the currently selected category
+ */
 export const getCategorySuggestion = (payee, selectedCategory) => state => {
   const transactions = getSortedTransactions(state)
-  const categories = select(CategoryResource).asArray(state)
+  const categories = categoriesAsArray(state)
   if (payee && !payee.startsWith('transfer:') && !selectedCategory) {
     return head(
       sort(
