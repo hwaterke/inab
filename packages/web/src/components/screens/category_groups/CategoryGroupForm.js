97e8c3c0d52865d45c69237825c093cd62a6c869
hwaterke <hwaterke@users.noreply.github.com>
2018-06-21 09:13:49 +0200

    Migrated category forms to final form

--- packages/web/src/components/screens/category_groups/CategoryGroupForm.js
+++ packages/web/src/components/screens/category_groups/CategoryGroupForm.js
@@ -1,51 +1,54 @@
-import {CategoryGroupResource} from '@inab/shared'
 import PropTypes from 'prop-types'
 import React from 'react'
-import {Field, reduxForm} from 'redux-form'
+import {Field, Form} from 'react-final-form'
 import {InputField} from '../../forms/fields/InputField'
 import {FormActionBar} from '../../forms/FormActionBar'
 import {required} from '../../forms/validations'
 
-@reduxForm({form: CategoryGroupResource.name})
 export class CategoryGroupForm extends React.Component {
   static propTypes = {
     isCreate: PropTypes.bool.isRequired,
     isUpdate: PropTypes.bool.isRequired,
-    handleSubmit: PropTypes.func.isRequired,
-    reset: PropTypes.func.isRequired,
-    pristine: PropTypes.bool.isRequired,
-    submitting: PropTypes.bool.isRequired,
     deleteResource: PropTypes.func,
+
+    onSubmit: PropTypes.func.isRequired,
+    initialValues: PropTypes.object,
   }
 
   render() {
     return (
-      <form onSubmit={this.props.handleSubmit}>
-        <Field
-          name="name"
-          component={InputField}
-          type="text"
-          label="Name"
-          validate={[required]}
-          required
-        />
+      <Form
+        onSubmit={this.props.onSubmit}
+        initialValues={this.props.initialValues}
+        render={({handleSubmit, form, pristine, submitting}) => (
+          <form onSubmit={handleSubmit}>
+            <Field
+              name="name"
+              component={InputField}
+              type="text"
+              label="Name"
+              validate={required}
+              required
+            />
 
-        <Field
-          name="priority"
-          component={InputField}
-          type="number"
-          label="Priority"
-        />
+            <Field
+              name="priority"
+              component={InputField}
+              type="number"
+              label="Priority"
+            />
 
-        <FormActionBar
-          handleSubmit={this.props.handleSubmit}
-          isCreate={this.props.isCreate}
-          isUpdate={this.props.isUpdate}
-          disableReset={this.props.pristine || this.props.submitting}
-          reset={this.props.reset}
-          remove={this.props.deleteResource}
-        />
-      </form>
+            <FormActionBar
+              handleSubmit={handleSubmit}
+              isCreate={this.props.isCreate}
+              isUpdate={this.props.isUpdate}
+              disableReset={pristine || submitting}
+              reset={() => form.reset()}
+              remove={this.props.deleteResource}
+            />
+          </form>
+        )}
+      />
     )
   }
 }
