404c1f552762e7d54170c4cb8f7c81f486c113d7
hwaterke <hwaterke@users.noreply.github.com>
2019-02-21 21:19:13 +0100

    Now using node 10

--- packages/server/docker_build.sh
+++ packages/server/docker_build.sh
@@ -7,5 +7,5 @@ SHORT_COMMIT_REF=$(git rev-parse --short HEAD)
 
 yarn build
 
-docker pull node:9-alpine
+docker pull node:10-alpine
 docker build --no-cache -t hwaterke/inab-api:latest -t hwaterke/inab-api:${SHORT_COMMIT_REF} --build-arg server_version=${COMMIT_REF} .
