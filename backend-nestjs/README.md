1a43464252a4d3f92dd6cd4f985a529d9e2dd630
hwaterke <hwaterke@users.noreply.github.com>
2023-05-14 11:05:29 +0200

    Adds CLI commands

--- backend-nestjs/README.md
+++ backend-nestjs/README.md
@@ -33,3 +33,17 @@ $ npm run test:e2e
 # test coverage
 $ npm run test:cov
 ```
+
+## CLI
+
+The backend comes with a CLI.
+
+Before using it you need to build the project with `npm run build`.
+
+```bash
+# Create a new bank account
+./bin/run accounts create --name "ING" --iban "BE00000000000000"
+
+# List all bank accounts
+./bin/run accounts list
+```
---
+++ backend-nestjs/src/commands/accounts/add.ts
@@ -0,0 +1,34 @@
+import {Command, Flags} from '@oclif/core'
+import {NestFactory} from '@nestjs/core'
+import {AppModule} from '../../app.module'
+import {BankAccountService} from '../../bank-accounts/bank-account.service'
+
+export default class Add extends Command {
+  static description = 'add a bank account'
+
+  static flags = {
+    name: Flags.string({
+      required: true,
+      char: 'n',
+      description: 'name of the bank acocunt',
+    }),
+    iban: Flags.string({
+      required: true,
+      description: 'iban of the bank acocunt',
+    }),
+  }
+
+  public async run(): Promise<void> {
+    const {flags} = await this.parse(Add)
+
+    const app = await NestFactory.createApplicationContext(AppModule)
+    const accountsService = app.get(BankAccountService)
+
+    await accountsService.create({
+      name: flags.name,
+      iban: flags.iban,
+    })
+
+    this.log('Bank account created')
+  }
+}
