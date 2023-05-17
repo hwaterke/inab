005d794f574436bf679392727d2a81d58d27c228
hwaterke <hwaterke@users.noreply.github.com>
2023-05-17 18:59:13 +0200

    Improve transaction table

--- web-vite/src/routes/Transactions.tsx
+++ web-vite/src/routes/Transactions.tsx
@@ -2,6 +2,11 @@ import {graphql} from '../gql'
 import {useQuery} from '@apollo/client'
 import {useTitle} from '../utils/useTitle.ts'
 
+const formater = new Intl.NumberFormat('fr-FR', {
+  style: 'currency',
+  currency: 'EUR',
+})
+
 const allTransactionsQueryDocument = graphql(`
   query transactions {
     transactions {
@@ -30,16 +35,115 @@ export const Transactions = () => {
   const {data} = useQuery(allTransactionsQueryDocument)
 
   return (
-    <div>
-      {data && (
-        <ul role="list">
-          {data.transactions.map((transaction) => (
-            <li key={transaction.uuid} className="border p-2">
-              <pre>{JSON.stringify(transaction)}</pre>
-            </li>
-          ))}
-        </ul>
-      )}
-    </div>
+    <>
+      <div className="px-4 sm:px-6 lg:px-8">
+        <div className="sm:flex sm:items-center">
+          <div className="sm:flex-auto">
+            <h1 className="text-base font-semibold leading-6 text-gray-900">
+              Transactions
+            </h1>
+            <p className="mt-2 text-sm text-gray-700">
+              A table of placeholder stock market data that does not make any
+              sense.
+            </p>
+          </div>
+          <div className="mt-4 sm:ml-16 sm:mt-0 sm:flex-none">
+            <button
+              type="button"
+              className="block rounded-md bg-indigo-600 px-3 py-2 text-center text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+            >
+              Export
+            </button>
+          </div>
+        </div>
+        <div className="mt-8 flow-root">
+          <div className="-mx-4 -my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
+            <div className="inline-block min-w-full py-2 align-middle sm:px-6 lg:px-8">
+              <table className="min-w-full divide-y divide-gray-300">
+                <thead>
+                  <tr>
+                    <th
+                      scope="col"
+                      className="whitespace-nowrap py-3.5 pl-4 pr-3 text-left text-sm font-semibold text-gray-900 sm:pl-0"
+                    >
+                      Account
+                    </th>
+                    <th
+                      scope="col"
+                      className="whitespace-nowrap px-2 py-3.5 text-left text-sm font-semibold text-gray-900"
+                    >
+                      Date
+                    </th>
+                    <th
+                      scope="col"
+                      className="whitespace-nowrap px-2 py-3.5 text-left text-sm font-semibold text-gray-900"
+                    >
+                      Time
+                    </th>
+                    <th
+                      scope="col"
+                      className="whitespace-nowrap px-2 py-3.5 text-left text-sm font-semibold text-gray-900"
+                    >
+                      Payee
+                    </th>
+                    <th
+                      scope="col"
+                      className="whitespace-nowrap px-2 py-3.5 text-left text-sm font-semibold text-gray-900"
+                    >
+                      Category
+                    </th>
+                    <th
+                      scope="col"
+                      className="whitespace-nowrap px-2 py-3.5 text-right text-sm font-semibold text-gray-900"
+                    >
+                      Amount
+                    </th>
+                    <th
+                      scope="col"
+                      className="relative whitespace-nowrap py-3.5 pl-3 pr-4 sm:pr-0"
+                    >
+                      <span className="sr-only">Edit</span>
+                    </th>
+                  </tr>
+                </thead>
+                <tbody className="divide-y divide-gray-200 bg-white">
+                  {data?.transactions.map((transaction) => (
+                    <tr key={transaction.uuid}>
+                      <td className="whitespace-nowrap py-2 pl-4 pr-3 text-sm text-gray-500 sm:pl-0">
+                        {transaction.bankAccount.name}
+                      </td>
+                      <td className="whitespace-nowrap px-2 py-2 text-sm font-medium text-gray-900">
+                        {transaction.date}
+                      </td>
+                      <td className="whitespace-nowrap px-2 py-2 text-sm text-gray-900">
+                        {transaction.time}
+                      </td>
+                      <td className="whitespace-nowrap px-2 py-2 text-sm text-gray-500">
+                        {transaction.payee?.name}
+                      </td>
+                      <td className="whitespace-nowrap px-2 py-2 text-sm text-gray-500">
+                        {transaction.category?.name}
+                      </td>
+                      <td className="whitespace-nowrap px-2 py-2 text-sm text-gray-500 text-right">
+                        {formater.format(transaction.amount / 100)}
+                      </td>
+                      <td className="relative whitespace-nowrap py-2 pl-3 pr-4 text-right text-sm font-medium sm:pr-0">
+                        <a
+                          href="#"
+                          className="text-indigo-600 hover:text-indigo-900"
+                        >
+                          Edit
+                          <span className="sr-only">, {transaction.uuid}</span>
+                        </a>
+                      </td>
+                    </tr>
+                  ))}
+                </tbody>
+              </table>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
   )
 }
