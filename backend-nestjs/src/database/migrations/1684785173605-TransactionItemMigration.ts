a6e2d99fee9b4056f255de4b26845883994ba0c1
hwaterke <hwaterke@users.noreply.github.com>
2023-05-22 21:51:53 +0200

    No category for reimbursement and income

--- backend-nestjs/src/database/migrations/1684785173605-TransactionItemMigration.ts
+++ backend-nestjs/src/database/migrations/1684785173605-TransactionItemMigration.ts
@@ -1,9 +1,9 @@
 import {MigrationInterface, QueryRunner} from 'typeorm'
 
-export class TransactionItemMigration1684496078491
+export class TransactionItemMigration1684785173605
   implements MigrationInterface
 {
-  name = 'TransactionItemMigration1684496078491'
+  name = 'TransactionItemMigration1684785173605'
 
   public async up(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.query(
@@ -12,11 +12,20 @@ export class TransactionItemMigration1684496078491
            "uuid"             varchar PRIMARY KEY NOT NULL,
            "amount"           integer             NOT NULL,
            "transaction_uuid" varchar             NOT NULL,
-           "category_uuid"    varchar             NOT NULL,
+           "category_uuid"    varchar,
+           "is_income"        boolean             NOT NULL,
            "is_credit"        boolean             NOT NULL,
            "reimburse_uuid"   varchar,
            "created_at"       datetime            NOT NULL DEFAULT (datetime('now')),
            "updated_at"       datetime            NOT NULL DEFAULT (datetime('now')),
+           CONSTRAINT "CHK_2014030f1288f7c8781f9621e1" CHECK (amount <> 0),
+           CONSTRAINT "CHK_987309ba04f9007da96292eaf9" CHECK (is_credit IS FALSE OR amount < 0),
+           CONSTRAINT "CHK_60e2ab824c7a337367700d60ef" CHECK (reimburse_uuid IS NULL OR amount > 0),
+           CONSTRAINT "CHK_3495e200b5254756a55e6308da" CHECK (is_income IS FALSE OR amount > 0),
+           CONSTRAINT "CHK_1ba565fe74a448f1845a374fde" CHECK (reimburse_uuid IS NULL OR is_credit IS FALSE),
+           CONSTRAINT "CHK_24353ca6f2db508e1647758770" CHECK ((is_income IS TRUE AND reimburse_uuid IS NULL AND category_uuid IS NULL) OR
+                                                              (is_income IS FALSE AND reimburse_uuid IS NULL AND category_uuid IS NOT NULL) OR
+                                                              (is_income IS FALSE AND reimburse_uuid IS NOT NULL AND category_uuid IS NULL)),
            CONSTRAINT "FK_8d2b56cd2dc49b1c5d346809219" FOREIGN KEY ("transaction_uuid") REFERENCES "bank_transaction" ("uuid") ON DELETE NO ACTION ON UPDATE NO ACTION,
            CONSTRAINT "FK_72917b684db378ebdf75d3ab8c4" FOREIGN KEY ("category_uuid") REFERENCES "category" ("uuid") ON DELETE NO ACTION ON UPDATE NO ACTION,
            CONSTRAINT "FK_d85d2056aebc7ca81e919b85dc4" FOREIGN KEY ("reimburse_uuid") REFERENCES "bank_transaction_item" ("uuid") ON DELETE NO ACTION ON UPDATE NO ACTION
--- backend-nestjs/src/transactions/entities/bank-transaction-item.entity.ts
+++ backend-nestjs/src/transactions/entities/bank-transaction-item.entity.ts
@@ -1,4 +1,5 @@
 import {
+  Check,
   Column,
   CreateDateColumn,
   Entity,
@@ -12,6 +13,27 @@ import {Category} from '../../categories/entities/category.entity'
 import {BankTransaction} from './bank-transaction.entities'
 
 @Entity()
+// An item is either an income, a reimbursement or it has a category
+@Check(
+  [
+    // Income
+    '(is_income IS TRUE AND reimburse_uuid IS NULL AND category_uuid IS NULL)',
+    // Category
+    '(is_income IS FALSE AND reimburse_uuid IS NULL AND category_uuid IS NOT NULL)',
+    // Reimbursement
+    '(is_income IS FALSE AND reimburse_uuid IS NOT NULL AND category_uuid IS NULL)',
+  ].join(' OR ')
+)
+// If the item is a reimbursement, it cannot be a credit
+@Check(`reimburse_uuid IS NULL OR is_credit IS FALSE`)
+// If the item is an income, the amount must be positive
+@Check(`is_income IS FALSE OR amount > 0`)
+// If the item is a reimbursement, the amount must be positive
+@Check(`reimburse_uuid IS NULL OR amount > 0`)
+// If the item is a credit, the amount must be negative
+@Check(`is_credit IS FALSE OR amount < 0`)
+// Amount cannot be 0
+@Check(`amount <> 0`)
 export class BankTransactionItem {
   @PrimaryGeneratedColumn('uuid')
   uuid!: string
@@ -28,9 +50,13 @@ export class BankTransactionItem {
 
   @ManyToOne(() => Category)
   @JoinColumn({name: 'category_uuid'})
-  category!: Category
-  @Column('uuid', {name: 'category_uuid'})
-  categoryUuid!: string
+  category!: Category | null
+  @Column('uuid', {name: 'category_uuid', nullable: true})
+  categoryUuid!: string | null
+
+  // Flag to indicate if the amount is an income. An income does not have a category.
+  @Column('boolean', {name: 'is_income'})
+  isIncome!: boolean
 
   // Flag to indicate if the amount is a credit. A credit is an amount that is expected to be reimbursed.
   @Column('boolean', {name: 'is_credit'})
